20291f3f6bf972d1d89e3525a5289e9c2057cb61
bhattjayd <cbhatt526@gmail.com>
2023-09-17 19:45:33 +0530

    device discovery with ping

--- App.tsx
+++ App.tsx
@@ -5,114 +5,74 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
+import Ping from 'react-native-ping';
+import DeviceInfo from 'react-native-device-info';
+import {generateIPRange} from './src/utils/helper';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): JSX.Element {
+  const [hosts, setHosts] = useState<string[]>([]);
+  const [ActiveHosts, setActiveHosts] = useState<string[]>([]);
+  const [ip, setIp] = useState<string>(''); // Initialize to an empty string
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const getIp = async () => {
+    try {
+      const ipAddress = await DeviceInfo.getIpAddress();
+      setIp(ipAddress); // Set the IP address once it's retrieved
+    } catch (error) {
+      console.error('Error fetching IP address:', error);
+    }
+  };
+  useEffect(() => {
+    if (ip != '') {
+      console.log('ip');
+      console.log(generateIPRange(ip, 255).length);
+      setHosts(generateIPRange(ip, 255));
+    }
+  }, [ip]);
+  useEffect(() => {
+    if (hosts.length > 0) {
+      hosts.forEach(i => {
+        getData(i);
+      });
+    }
+  }, [hosts]);
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    if (ActiveHosts.length > 0) {
+      console.log(ActiveHosts);
+    }
+  }, [ActiveHosts]);
+
+  const getData = async (currentIp: string) => {
+    try {
+      const ms = await Ping.start(currentIp, {timeout: 1000});
+      console.log('ms', ms);
+      if (ms >= 0) {
+        setActiveHosts(prevActiveHosts => [...prevActiveHosts, currentIp]);
+        console.log(currentIp);
+
+        console.log(currentIp, ActiveHosts);
+      }
+    } catch (error: any) {
+      console.log('Error pinging host:', error);
+    }
+  };
+
+  useEffect(() => {
+    getIp();
+  }, []);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      {ActiveHosts.map((host, index) => (
+        <Text key={index} style={{color: 'red'}}>
+          {host}
+        </Text>
+      ))}
     </View>
   );
 }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
